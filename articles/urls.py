0ae6e57202c393561e00e3e540e2fc646dd9f03b
Александр Агакин <agakinalexandr@gmail.com>
2023-03-12 10:50:35 +0100

    creating article

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import (ArticleListView, ArticleDeleteView, ArticleDetailView,
-                    ArticleEditView)
+                    ArticleEditView, ArticleCreateView)
 
 urlpatterns = [
     path('<int:pk>/edit/', ArticleEditView.as_view(), name='article_edit'),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(),
          name='article_delete'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('', ArticleListView.as_view(), name='article_list')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import DeleteView, UpdateView
 from .models import Article
 
@@ -7,6 +7,7 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
+    ordering = ['-date']
 
 
 class ArticleDetailView(DetailView):
@@ -24,3 +25,9 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body', 'author',)
